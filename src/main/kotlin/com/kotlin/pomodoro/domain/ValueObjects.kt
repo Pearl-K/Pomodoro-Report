4a574aae901759a47c7a9105d92863fffd78e25c
강진주 <rkdwlswn227@gmail.com>
2025-08-07 13:19:14 +0900

    Initial commit

---
+++ src/main/kotlin/com/kotlin/pomodoro/domain/ValueObjects.kt
@@ -0,0 +1,5 @@
+package com.kotlin.pomodoro.domain
+
+@JvmInline value class UserId(val value: Long)
+@JvmInline value class PomodoroSessionId(val value: Long)
+@JvmInline value class PomodoroReportId(val value: Long)
